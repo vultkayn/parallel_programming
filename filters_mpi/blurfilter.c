d14824de867f985805e29a63ad0aa5510c1f040b
benjamin priour <benjamin.priour@grenoble-inp.org>
2023-04-24 20:51:46 +0200

    allocations are now only on world_rank 0, and time is taken with MPI_Wtime

--- filters_mpi/blurfilter.c
+++ filters_mpi/blurfilter.c
@@ -2,141 +2,140 @@
   File: blurfilter.c
   Implementation of blurfilter function.
  */
- 
+
 #include "blurfilter.h"
 
-pixel* pix(pixel* image, const int xx, const int yy, const int xsize)
+pixel *pix(pixel *image, const int xx, const int yy, const int xsize)
 {
-	int off = xsize*yy + xx;
+	int off = xsize * yy + xx;
 	return (image + off);
 }
 
 void blurfilter(const int xsize, const int ysize, const int world_rank, int world_size, pixel *src, double *w, int radius)
-{	
-	MPI_Datatype MPI_PIXEL;
-  	MPI_Type_contiguous(3, MPI_UNSIGNED_CHAR, &MPI_PIXEL);
-  	MPI_Type_commit(&MPI_PIXEL);
-	
+{
+
 	/* Row */
-	
-	int* displs = (int *)malloc(sizeof(int)*(world_size));
-	int* chunksize = (int*) malloc(sizeof(int)*(world_size));
+
+	int displs[world_size];
+	int chunksize[world_size];
 	split_workload(chunksize, displs, xsize, ysize, world_size);
 	int send_size = chunksize[world_rank];
-	pixel* rcv = (pixel*) malloc(sizeof(pixel) * send_size);
-	pixel* rcvb = (pixel*) malloc(sizeof(pixel) * send_size);
+	pixel *rcv = (pixel *)  malloc(sizeof (pixel) * send_size);
+	pixel *rcvb = (pixel *) malloc(sizeof (pixel) * send_size);
 
-	if(world_rank==0)
+	if (world_rank == 0)
 	{
-		if(world_size>1){
+		if (world_size > 1)
+		{
 			printf("Filtering with %d processes.\n", world_size);
 		}
-		else{
+		else
+		{
 			puts("Sequential execution.");
 		}
 	}
 
-	MPI_Scatterv(src, chunksize, displs, MPI_PIXEL, rcv, send_size, MPI_PIXEL, 0, MPI_COMM_WORLD);
-	blurfilter_row(xsize, (chunksize[world_rank]/xsize), rcv, rcvb, radius, w);
-	MPI_Gatherv(rcvb, send_size, MPI_PIXEL, src, chunksize, displs, MPI_PIXEL, 0, MPI_COMM_WORLD);
+	MPI_Datatype MPI_PIXEL;
+	MPI_Type_contiguous(3, MPI_UNSIGNED_CHAR, &MPI_PIXEL);
+	MPI_Type_commit(&MPI_PIXEL);
+	MPI_Scatterv (src, chunksize, displs, MPI_PIXEL, rcv, send_size, MPI_PIXEL, 0, MPI_COMM_WORLD);
+	blurfilter_row (xsize, (chunksize[world_rank] / xsize), rcv, rcvb, radius, w);
+	MPI_Gatherv (rcvb, send_size, MPI_PIXEL, src, chunksize, displs, MPI_PIXEL, 0, MPI_COMM_WORLD);
 
 	/* Column */
 
 	split_workload(chunksize, displs, ysize, xsize, world_size);
-	send_size = chunksize[world_rank];
 
-	pixel* dst = (pixel*) malloc(sizeof(pixel) * xsize * ysize);
-	pixel* rcv2 = (pixel*) malloc(sizeof(pixel) * send_size);
-	pixel* rcvb2 = (pixel*) malloc(sizeof(pixel) * send_size);
-	
-	if(world_rank==0)
+	pixel *dst = NULL;
+
+	if (world_rank == 0)
 	{
-		rotate(dst,src,xsize,ysize,0);
-	}	
-
-	MPI_Scatterv(dst, chunksize, displs, MPI_PIXEL, rcv2, send_size, MPI_PIXEL, 0, MPI_COMM_WORLD);
-	blurfilter_row(ysize, (chunksize[world_rank]/ysize), rcv2, rcvb2, radius, w);
-	MPI_Gatherv(rcvb2, send_size, MPI_PIXEL, dst, chunksize, displs, MPI_PIXEL, 0, MPI_COMM_WORLD);
-
-	if(world_rank==0) 
-	{
-		rotate(src,dst,xsize,ysize,1);
+		dst = (pixel *) malloc(sizeof(pixel) * xsize * ysize);
+		rotate(dst, src, xsize, ysize, 0);
 	}
 
-	free(displs);
-	free(chunksize);
+	send_size = chunksize[world_rank];
+	pixel *rcv2 = (pixel *) malloc(sizeof(pixel) * send_size);
+	pixel *rcvb2 = (pixel *) malloc(sizeof(pixel) * send_size);
+	MPI_Scatterv(dst, chunksize, displs, MPI_PIXEL, rcv2, send_size, MPI_PIXEL, 0, MPI_COMM_WORLD);
+	blurfilter_row(ysize, (chunksize[world_rank] / ysize), rcv2, rcvb2, radius, w);
+	MPI_Gatherv(rcvb2, send_size, MPI_PIXEL, dst, chunksize, displs, MPI_PIXEL, 0, MPI_COMM_WORLD);
+
+	if (world_rank == 0)
+	{
+		rotate(src, dst, xsize, ysize, 1);
+	}
+
 	free(rcv);
 	free(rcvb);
 	free(dst);
 	free(rcv2);
 	free(rcvb2);
-}     
+}
 
-void split_workload(int* chunksize, int* displs, const int size, const int split, const int world_size)
+void split_workload(int *chunksize, int *displs, const int size, const int split, const int world_size)
 {
-	int step = floor(split/world_size);
+	int step = floor(split / world_size);
 
-	displs[0] = 0; 
+	displs[0] = 0;
 
-	for(int k=0;k<(world_size-1);k++)
-	{ 
-		chunksize[k] = step*size;
+	for (int k = 0; k < (world_size - 1); k++)
+	{
+		chunksize[k] = step * size;
 	}
 
-	chunksize[world_size-1] = (split - step*(world_size-1))*size;
+	chunksize[world_size - 1] = (split - step * (world_size - 1)) * size;
 
-	for(int j=1;j<world_size;j++)
+	for (int j = 1; j < world_size; j++)
 	{
-		displs[j] = displs[j-1] + chunksize[j-1];
+		displs[j] = displs[j - 1] + chunksize[j - 1];
 	}
 }
 
-void rotate(pixel* output, pixel* input, const int xsize, const int ysize, const int clockwise)
+void rotate(pixel *output, pixel *input, const int xsize, const int ysize, const int clockwise)
 {
 	int x2, y2, x0, y0;
-	for(int yy=0; yy<ysize; yy++)
+	for (int yy = 0; yy < ysize; yy++)
 	{
-		for(int xx=0; xx<xsize; xx++)
+		for (int xx = 0; xx < xsize; xx++)
 		{
-			if(clockwise==0) // + 90° rotation.
+			if (clockwise == 0) // + 90° rotation.
 			{
 				x2 = yy;
-				y2 = -xx + (xsize-1);
+				y2 = -xx + (xsize - 1);
 
-				pix(output,x2,y2,ysize)->r=pix(input,xx,yy,xsize)->r;
-				pix(output,x2,y2,ysize)->g=pix(input,xx,yy,xsize)->g;
-				pix(output,x2,y2,ysize)->b=pix(input,xx,yy,xsize)->b;
-			}	
-			
-			if(clockwise==1) // - 90° rotation.
+				pix(output, x2, y2, ysize)->r = pix(input, xx, yy, xsize)->r;
+				pix(output, x2, y2, ysize)->g = pix(input, xx, yy, xsize)->g;
+				pix(output, x2, y2, ysize)->b = pix(input, xx, yy, xsize)->b;
+			}
+
+			if (clockwise == 1) // - 90° rotation.
 			{
 				x2 = yy;
-				y2 = -xx + (xsize-1);
+				y2 = -xx + (xsize - 1);
 
-				pix(output,xx,yy,xsize)->r=pix(input,x2,y2,ysize)->r;
-				pix(output,xx,yy,xsize)->g=pix(input,x2,y2,ysize)->g;
-				pix(output,xx,yy,xsize)->b=pix(input,x2,y2,ysize)->b;
-			}	
-			
-			
+				pix(output, xx, yy, xsize)->r = pix(input, x2, y2, ysize)->r;
+				pix(output, xx, yy, xsize)->g = pix(input, x2, y2, ysize)->g;
+				pix(output, xx, yy, xsize)->b = pix(input, x2, y2, ysize)->b;
+			}
 		}
-	}	
+	}
 }
 
-void blurfilter_row(const int xsize, const int ysize, pixel* src, pixel* dst, const int radius, const double *w)
+void blurfilter_row(const int xsize, const int ysize, pixel *src, pixel *dst, const int radius, const double *w)
 {
 	int x, y, x2, y2, wi;
 	double r, g, b, n, wc;
 
-	for (y=0; y<ysize; y++)
+	for (y = 0; y < ysize; y++)
 	{
-		for (x=0; x<xsize; x++)
+		for (x = 0; x < xsize; x++)
 		{
 			r = w[0] * pix(src, x, y, xsize)->r;
 			g = w[0] * pix(src, x, y, xsize)->g;
 			b = w[0] * pix(src, x, y, xsize)->b;
 			n = w[0];
-			for ( wi=1; wi <= radius; wi++)
+			for (wi = 1; wi <= radius; wi++)
 			{
 				wc = w[wi];
 				x2 = x - wi;
@@ -156,9 +155,9 @@ void blurfilter_row(const int xsize, const int ysize, pixel* src, pixel* dst, co
 					n += wc;
 				}
 			}
-			pix(dst,x,y, xsize)->r = r/n;
-			pix(dst,x,y, xsize)->g = g/n;
-			pix(dst,x,y, xsize)->b = b/n;
+			pix(dst, x, y, xsize)->r = r / n;
+			pix(dst, x, y, xsize)->g = g / n;
+			pix(dst, x, y, xsize)->b = b / n;
 		}
 	}
 }
--- filters_mpi/blurmain.c
+++ filters_mpi/blurmain.c
@@ -10,6 +10,12 @@
 
 #define MAX_RAD 1000
 
+struct img_info {
+	unsigned radius;
+	unsigned xsize;
+	unsigned ysize;
+};
+
 int main (int argc, char ** argv)
 {
 	MPI_Init(NULL, NULL);
@@ -20,13 +26,18 @@ int main (int argc, char ** argv)
 	int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	int radius, xsize, ysize;
-	struct timespec stime, etime;
+	struct img_info img_info;
+	MPI_Datatype MPI_IMG_INFO;
+	MPI_Type_contiguous(3, MPI_UNSIGNED, &MPI_IMG_INFO);
+	MPI_Type_commit(&MPI_IMG_INFO);
+	// struct timespec stime, etime;
+	double stime, etime;
 	double w[MAX_RAD];
-	pixel *src = (pixel*) malloc(sizeof(pixel) * MAX_PIXELS);
 
+	pixel *src = NULL;
 	if(world_rank==0)
 	{
+		src = (pixel*) malloc(sizeof(pixel) * MAX_PIXELS);
 		int colmax;
 		
 		/* Take care of the arguments */
@@ -37,16 +48,16 @@ int main (int argc, char ** argv)
 			exit(1);
 		}
 		
-		radius = atoi(argv[1]);
-		if ((radius > MAX_RAD) || (radius < 1))
+		img_info.radius = atoi(argv[1]);
+		if ((img_info.radius > MAX_RAD) || (img_info.radius < 1))
 		{
-			fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
+			fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", img_info.radius, MAX_RAD);
 			exit(1);
 		}
 
 		/* Read file */
 
-		if (read_ppm (argv[2], &xsize, &ysize, &colmax, (char *) src) != 0)
+		if (read_ppm (argv[2], &img_info.xsize, &img_info.ysize, &colmax, (char *) src) != 0)
 			exit(1);
 		
 		if (colmax > 255)
@@ -59,28 +70,29 @@ int main (int argc, char ** argv)
 		
 		/* Filtering */
 
-		get_gauss_weights(radius, w);
+		get_gauss_weights(img_info.radius, w);
 		
 		printf("Calling filter\n");
 
 		clock_gettime(CLOCK_REALTIME, &stime);
+		stime = MPI_Wtime();
 	}
 
-	MPI_Bcast(&xsize,1,MPI_INT,0,MPI_COMM_WORLD);
-	MPI_Bcast(&ysize,1,MPI_INT,0,MPI_COMM_WORLD);
-	MPI_Bcast(&radius,1,MPI_INT,0,MPI_COMM_WORLD);
-	MPI_Bcast(w,(radius+1),MPI_DOUBLE,0,MPI_COMM_WORLD);
+	MPI_Bcast(&img_info, 1, MPI_IMG_INFO, 0, MPI_COMM_WORLD);
+	MPI_Bcast(w, (img_info.radius+1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	blurfilter(xsize, ysize, world_rank, world_size, src, w, radius);
+	blurfilter(img_info.xsize, img_info.ysize, world_rank, world_size, src, w, img_info.radius);
 	
 	if(world_rank==0)
 	{		
-		clock_gettime(CLOCK_REALTIME, &etime);
-	
-		printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) + 1e-9*(etime.tv_nsec - stime.tv_nsec)) ;
+		// clock_gettime(CLOCK_REALTIME, &etime);
+		etime = MPI_Wtime();
+
+		// printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) + 1e-9*(etime.tv_nsec - stime.tv_nsec)) ;
+		printf("Filtering took: %g secs\n", (etime  - stime)) ;
 	
 		/* Write result */
-		if(write_ppm(argv[3], xsize, ysize, (char *)src) != 0)
+		if(write_ppm(argv[3], img_info.xsize, img_info.ysize, (char *)src) != 0)
 			exit(1);
 
 		printf("Writing output file\n");
